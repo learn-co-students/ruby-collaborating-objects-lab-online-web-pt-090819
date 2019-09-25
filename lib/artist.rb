f03aa17eeecaee05b7fb9c5660f62858a6626ce0
Chaya Deaver <chayadeaver@gmail.com>
2019-09-25 13:59:55 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+    @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self }
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+end
