80a086602e5bd515bb767c250e0e9bd33f24a73b
Lauren Dove <elledoveinc@gmail.com>
2019-11-05 02:24:27 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+  
+    Dir.entries(path).select{|entrie| entrie.include?(".mp3")}
+    
+  end
+
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+  end
+
+end
